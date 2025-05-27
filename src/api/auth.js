c8b3059a09d5834200a5381cf3526e71fa2df596
Niksaaa <Niksaaa@HOME-PC>
2025-05-27 23:46:41 +0300

    updated

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = '${process.env.REACT_APP_API_URL}/api/Auth/login';
+const API_URL = '${process.env.REACT_APP_API_URL}/api/Auth';
 
 // Create axios instance with base configuration
 const authApi = axios.create({
